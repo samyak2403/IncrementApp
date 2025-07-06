731d3c143a489cac6391fcd8740cc575ea799079
Samyak Kamble <samyakkamble6659@gmail.com>
2025-07-06 22:22:48 +0530

    ads added

--- app/src/main/java/com/samyak2403i/incrementapp/MainActivity.java
+++ app/src/main/java/com/samyak2403i/incrementapp/MainActivity.java
@@ -29,6 +29,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+// Import Start.io SDK
+import com.startapp.sdk.adsbase.StartAppAd;
+import com.startapp.sdk.adsbase.StartAppSDK;
+import com.startapp.sdk.adsbase.adlisteners.AdDisplayListener;
+import com.startapp.sdk.adsbase.adlisteners.AdEventListener;
+import com.startapp.sdk.adsbase.StartAppAd.AdMode;
+import com.startapp.sdk.adsbase.AutoInterstitialPreferences;
+import com.startapp.sdk.ads.banner.Banner;
+import com.startapp.sdk.ads.banner.BannerListener;
 
 /**
  * MainActivity class that manages the UI and handles user interactions.
@@ -44,11 +56,30 @@ public class MainActivity extends AppCompatActivity {
 
     // Declare an integer variable to keep track of the number
     private int number = 0;
+    
+    // Declare StartAppAd for interstitial ads
+    private StartAppAd startAppAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Initialize the Start.io SDK with your App ID
+        StartAppSDK.init(this, "206321451", false);
+        
+        // For GDPR compliance - optional
+        StartAppSDK.setUserConsent(this, 
+                "pas",
+                System.currentTimeMillis(), 
+                true);
+        
+        // Enable return ads - using the correct method
+        StartAppAd.showAd(this);
+        
         setContentView(R.layout.activity_main);  // Set the layout file for this activity
+
+        // Initialize the StartAppAd object for interstitial ads
+        startAppAd = new StartAppAd(this);
 
         // Initialize the TextView and Buttons by finding their IDs in the layout
         numberTextView = findViewById(R.id.numberTextView);
@@ -61,6 +92,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Call the method to increment the number
                 incrementNumber();
+                
+                // Show interstitial ad every 5th click
+                if (number % 5 == 0) {
+                    showInterstitial();
+                }
             }
         });
 
@@ -72,6 +108,9 @@ public class MainActivity extends AppCompatActivity {
                 decrementNumber();
             }
         });
+        
+        // Add banner ad at the bottom of the screen
+        setupBannerAd();
     }
 
     /**
@@ -93,5 +132,87 @@ public class MainActivity extends AppCompatActivity {
         }
         // Update the TextView with the new number
         numberTextView.setText(String.valueOf(number));
+    }
+    
+    /**
+     * Method to set up a banner ad at the bottom of the screen
+     */
+    private void setupBannerAd() {
+        // Create a banner ad
+        Banner banner = new Banner(this);
+        
+        // Add the banner to the layout
+        RelativeLayout bannerLayout = findViewById(R.id.bannerAdContainer);
+        if (bannerLayout != null) {
+            bannerLayout.addView(banner);
+        }
+        
+        // Set banner listener for lifecycle events
+        banner.setBannerListener(new BannerListener() {
+            @Override
+            public void onReceiveAd(View view) {
+                // Banner has been received
+            }
+
+            @Override
+            public void onFailedToReceiveAd(View view) {
+                // Banner failed to load
+            }
+
+            @Override
+            public void onImpression(View view) {
+                // Banner impression
+            }
+
+            @Override
+            public void onClick(View view) {
+                // Banner clicked
+            }
+        });
+    }
+    
+    /**
+     * Method to show interstitial ad
+     */
+    private void showInterstitial() {
+        startAppAd.loadAd(AdMode.FULLPAGE, new AdEventListener() {
+            @Override
+            public void onReceiveAd(com.startapp.sdk.adsbase.Ad ad) {
+                // Ad loaded successfully, show it
+                startAppAd.showAd(new AdDisplayListener() {
+                    @Override
+                    public void adHidden(com.startapp.sdk.adsbase.Ad ad) {
+                        // Ad hidden
+                    }
+
+                    @Override
+                    public void adDisplayed(com.startapp.sdk.adsbase.Ad ad) {
+                        // Ad displayed
+                    }
+
+                    @Override
+                    public void adClicked(com.startapp.sdk.adsbase.Ad ad) {
+                        // Ad clicked
+                    }
+
+                    @Override
+                    public void adNotDisplayed(com.startapp.sdk.adsbase.Ad ad) {
+                        // Ad not displayed
+                    }
+                });
+            }
+
+            @Override
+            public void onFailedToReceiveAd(com.startapp.sdk.adsbase.Ad ad) {
+                // Failed to load ad
+            }
+        });
+    }
+    
+    @Override
+    public void onBackPressed() {
+        // Show an ad when the back button is pressed
+        startAppAd.onBackPressed();
+        super.onBackPressed();
     }
 }
